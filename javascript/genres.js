dd12415d904e60ae3a6089ec719955cbeb660138
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-10-13 20:03:30 +0300

    update

--- javascript/genres.js
+++ javascript/genres.js
@@ -2,6 +2,11 @@ import { removeOverlay } from './Components/header.js'
 import { handleCardClick , _moviePopup, _iframe  } from './Components/movie-popup.js';
 
 
+// Loading
+const loadingOverlay = document.getElementById('loading-overlay');
+if (loadingOverlay)
+    loadingOverlay.style.display = 'none';
+
 const _overlay = document.getElementById("overlay");
 
 _overlay.addEventListener("click", () => {
