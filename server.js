129d0aa277691d5da7daf85829c5d93beb1e0472
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-10-26 14:35:48 +0300

    x

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000; // You can use any port you like
+const PORT = process.env.PORT || 3000;
 const bearerToken = process.env.bearerToken;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Optionally, you can handle the root URL to serve the index.html directly
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/ping', (req, res) => {
+    res.json({ msg: "pong" });
+});
+
 app.get('/api/token', (req, res) => {
-    res.json({ bearerToken }); // Send the token as JSON
+    res.json({ bearerToken });
 });
 
 // Start the server
