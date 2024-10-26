55c0ce9f5c12c1125994e430934f1e6dce3e219d
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-10-26 14:46:52 +0300

    x

--- api/token.js
+++ api/token.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 export default function handler(req, res) {
     const bearerToken = process.env.bearerToken; // Access your environment variable
     res.status(200).json({ bearerToken }); // Send the token as JSON
